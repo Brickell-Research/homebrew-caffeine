32743341f694cf94900545c2e8901c2e4aa9454e
robertDurst <me@robdurst.com>
2025-11-19 21:33:49 -0700

    Update CaffeineLang formula to version 0.1.6 with new homepage and updated download URLs and SHA256 checksums for macOS and Linux binaries.

--- Formula/caffeine_lang.rb
+++ Formula/caffeine_lang.rb
@@ -1,19 +1,19 @@
 class CaffeineLang < Formula
   desc "Caffeine programming language"
-  homepage "https://github.com/Brickell-Research/caffeine_lang"
-  version "0.1.5"
+  homepage "https://caffeine-lang.run"
+  version "0.1.6"
   license "GPL-3.0-only"
 
   # Platform-specific downloads
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/Brickell-Research/caffeine_lang/releases/download/v0.1.5/caffeine-0.1.5-macos-x64.tar.gz"
-    sha256 "b85a32ba2d747a353f0080c9560aebe2f982d080aebfd56ae42f586f22ba4903"
+    url "https://github.com/Brickell-Research/caffeine_lang/releases/download/v0.1.6/caffeine-0.1.6-macos-x64.tar.gz"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/Brickell-Research/caffeine_lang/releases/download/v0.1.5/caffeine-0.1.5-macos-arm64.tar.gz"
-    sha256 "9ba8bd584e55eb97e8070e9073842107234220277f0984e0950509506c154741"
+    url "https://github.com/Brickell-Research/caffeine_lang/releases/download/v0.1.6/caffeine-0.1.6-macos-arm64.tar.gz"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/Brickell-Research/caffeine_lang/releases/download/v0.1.5/caffeine-0.1.5-linux-x64.tar.gz"
-    sha256 "412c6a57783824a3f236a42f6989865ce935a036d9f068a19a1aac5607e4b87e"
+    url "https://github.com/Brickell-Research/caffeine_lang/releases/download/v0.1.6/caffeine-0.1.6-linux-x64.tar.gz"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   end
 
   def install
